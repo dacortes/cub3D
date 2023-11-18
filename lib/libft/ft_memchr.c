bba6bc4141231f2ff8a0572b77673587292553be
dacortes <dannyel.cortesr@gmail.com>
2023-11-18 12:24:45 +0100

    [ADD] libft

---
+++ lib/libft/ft_memchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dacortes <dacortes@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/22 11:20:55 by dacortes          #+#    #+#             */
+/*   Updated: 2023/07/13 09:59:57 by dacortes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (((unsigned char *)s)[i] == (unsigned char)c)
+			return (((unsigned char *)s) + i);
+		i++;
+	}
+	return (NULL);
+}
---
+++ lib/libft/get_next_line_bonus.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dacortes <dacortes@student.42barcelona.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/17 10:45:10 by dacortes          #+#    #+#             */
+/*   Updated: 2023/07/13 10:06:00 by dacortes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include "libft.h"
+# include <limits.h>
+# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 600
+# endif
+
+# ifndef OPEN_MAX
+#  define OPEN_MAX 10240
+# endif
+
+char	*ft_free(char **buff, char **buff2);
+
+char	*get_next_line(int fd);
+char	*ft_next_buff(char *buf);
+char	*ft_next_line(char *buf, char *line);
+char	*ft_read_fd(int fd, char *buf, int bytes);
+
+char	*ft_protec_join(char *buffer, char *reader);
+#endif
