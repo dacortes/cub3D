450b30021b2f00d1ddc4359dc6be0d9ded2aa295
dacortes <dannyel.cortesr@gmail.com>
2023-11-21 11:34:34 +0100

    [IMP] struc data: no, so, we, ea

--- src/main.c
+++ src/main.c
@@ -6,15 +6,28 @@
 /*   By: dacortes <dacortes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 10:57:07 by dacortes          #+#    #+#             */
-/*   Updated: 2023/11/21 08:59:52 by dacortes         ###   ########.fr       */
+/*   Updated: 2023/11/21 11:30:09 by dacortes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/cub3D.h"
 
+int	clear_data(t_get_data *data)
+{
+	if (data->no)
+		free(data->no);
+	if (data->so)
+		free(data->so);
+	if (data->we)
+		free(data->we);
+	if (data->ea)
+		free(data->ea);
+	return (EXIT_SUCCESS);
+}
+
 int	main(int ac, char **av)
 {
-	t_get_data	text;
+	t_get_data	data;
 	char *line;
 	int	fd;
 
@@ -22,7 +35,8 @@ int	main(int ac, char **av)
 	if (ac != 2)
 		exit (msg_error(ARG, -1, NULL));
 	parse_open(av[1], &fd);
-	parse_data(&line, fd, &text);	
+	parse_data(&line, fd, &data);
+	clear_data(&data);
 	if (fd >= 0)
 		close (fd);
 	return (EXIT_SUCCESS);
--- src/parsing/getpath.c
+++ src/parsing/getpath.c
@@ -6,7 +6,7 @@
 /*   By: dacortes <dacortes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/20 17:39:22 by codespace         #+#    #+#             */
-/*   Updated: 2023/11/21 10:49:11 by dacortes         ###   ########.fr       */
+/*   Updated: 2023/11/21 11:33:49 by dacortes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 int	parse_open(char *file, int *fd)
 {
 	if (access(file, R_OK) == ERROR)
-			exit (msg_error(PRR, -1, file));
+		exit (msg_error(PRR, -1, file));
 	*fd = open(file, O_RDONLY);
 	if (*fd == ERROR)
 		exit (msg_error(OPN, -1, file));
 	return (EXIT_SUCCESS);
 }
 
-int search_get_data(char **set, char *line, int i, char *find)
+int	search_get_data(char **set, char *line, int i, char *find)
 {
 	int	len;
 
@@ -42,10 +42,9 @@ int search_get_data(char **set, char *line, int i, char *find)
 				exit (msg_error(MEM, -1, NULL));
 			if (!*set[0] || (*set[0] == '\n'))
 				exit (msg_error(MAP, -1, "path not found"));
-			ft_printf("*%s*\n", *set);
+			ft_printf("%s=*%s*\n", find, *set);
 			if (!*set[0])
 				exit (msg_error(MAP, -1, find));
-			free(*set);
 			return (1);
 		}
 	}
@@ -63,14 +62,14 @@ int	get_get_data(char *line, t_get_data *data, t_aux *chk)
 		i++;
 	if (line[i] && line[i + 1])
 	{
-		chk->no += search_get_data(&data->we, line, i, "NO");
-		chk->so += search_get_data(&data->we, line, i, "SO");
-		chk->we	+= search_get_data(&data->we, line, i, "WE");
-		chk->ea	+= search_get_data(&data->we, line, i, "EA");
+		chk->no += search_get_data(&data->no, line, i, "NO");
+		chk->so += search_get_data(&data->so, line, i, "SO");
+		chk->we += search_get_data(&data->we, line, i, "WE");
+		chk->ea += search_get_data(&data->ea, line, i, "EA");
 		if (chk->no > 1 || chk->so > 1 || chk->we > 1 || chk->ea > 1)
 			exit (msg_error(MAP, -1, "many textures"));
 		while (line[i] && is_space(line[i]))
-			i++;	
+			i++;
 	}
 	return (EXIT_SUCCESS);
 }
