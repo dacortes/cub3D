d83d3157e920cfa5e40164663173e4738a603135
dacortes <dannyel.cortesr@gmail.com>
2023-11-23 13:18:50 +0100

    [IMP] find color && aux

--- src/parsing/getcolor.c
+++ src/parsing/getcolor.c
@@ -6,27 +6,52 @@
 /*   By: dacortes <dacortes@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 19:04:25 by dacortes          #+#    #+#             */
-/*   Updated: 2023/11/23 12:38:32 by dacortes         ###   ########.fr       */
+/*   Updated: 2023/11/23 13:18:21 by dacortes         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/cub3D.h"
 
-int	is_num()
+int	is_num(char *str)
 {
-	return (EXIT_SUCCESS)
+	int	iter;
+
+	iter = 0;
+	while (str[iter])
+	{
+		if (!ft_isdigit(str[iter]))
+			return (FALSE);
+		iter++;
+	}
+	return (TRUE);
 }
 
 int	ignore_space(char *str, int *iter)
 {
-	while (str[*iter] && is_space(str[*iter]) && str[*iter] != '\n')
+	while (str[*iter] && is_space(str[*iter]))
 		++(*iter);
 	return (EXIT_SUCCESS);
 }
 
-int	find_color(t_color *set, t_aux *chk, int *len, int color)
+int	check_comma(t_aux *chk, int *len, int flag)
 {
-	(void)set;
+	int	comma;
+
+	comma = 0;
+	while (chk->line[*len] && chk->line[*len] == ',')
+	{
+		++comma;
+		++(*len);
+	}
+	if (!flag && comma != 1)
+		exit (msg_error(MAP, -1, "invalid color"));
+	if (flag && comma != 0)
+		exit (msg_error(MAP, -1, "invalid syntax"));
+	return (EXIT_SUCCESS);
+}
+
+int	find_color(int *set, t_aux *chk, int *len)
+{
 	char	*cpy;
 	int		iter;
 
@@ -35,14 +60,20 @@ int	find_color(t_color *set, t_aux *chk, int *len, int color)
 	if (chk->line[*len])
 	{
 		while (chk->line[iter] && !is_space(chk->line[iter])
-				&& chk->line[iter] != '\n' && chk->line[iter] != ',')
+			&& chk->line[iter] != '\n' && chk->line[iter] != ',')
 			++iter;
 		cpy = ft_strndup(&chk->line[*len], (iter - *len));
 		if (!cpy)
 			exit (msg_error(MEM, -1, NULL));
-		if (color == RED)
-			ft_printf("%s\n", cpy);
-		free(cpy);
+		if (!is_num(cpy) || !*cpy)
+			exit (msg_error(MAP, -1, "invalid color"));
+		*set = ft_atoi(cpy);
+		if (*set > 255)
+			exit (msg_error(MAP, -1, "invalid color: out of range"));
+		ft_printf("set color: %d\n", *set);
+		if (cpy)
+			free(cpy);
+		*len += (iter - *len);
 	}
 	return (EXIT_SUCCESS);
 }
@@ -50,7 +81,7 @@ int	find_color(t_color *set, t_aux *chk, int *len, int color)
 int	get_color(t_color *set, t_aux *chk, char *find, int *stt)
 {
 	int	len;
-	(void)set;
+
 	if (!ft_strncmp(&chk->line[chk->iter], find, 1))
 	{
 		(*stt)++;
@@ -64,7 +95,12 @@ int	get_color(t_color *set, t_aux *chk, char *find, int *stt)
 			while (chk->line[len] && !is_space(chk->line[len])
 				&& chk->line[len] != '\n')
 				++len;
-			find_color(set, chk, &len, RED);
+			find_color(&set->red, chk, &len);
+			check_comma(chk, &len, 0);
+			find_color(&set->green, chk, &len);
+			check_comma(chk, &len, 0);
+			find_color(&set->blue, chk, &len);
+			check_comma(chk, &len, 1);
 			return (TRUE);
 		}
 	}
