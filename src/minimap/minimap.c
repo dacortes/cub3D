e5dd45d5925d846383621fe019e21051a480e015
Joan Renau valls <jrenau-v@car14s3.42barcelona.com>
2023-11-24 14:52:00 +0100

    Some ajustments

--- src/minimap/minimap.c
+++ src/minimap/minimap.c
@@ -1,28 +1,18 @@
 #include "../../inc/cub3D.h"
 #include <stdio.h>
 
-int minimap_draw_square(t_minimap *minimap, t_point coord, int width, int height)
+int minimap_draw_square(t_minimap *minimap, t_point pixel, int width, int height)
 {
 	int		i;
 	int		j;
 
-//	row = minimap.offset.x + row;
-//	col = minimap.offset.y + col;
-	if (coord.x > minimap->cols || coord.y > minimap->rows)
-	{
-		printf("error on draw square inp\n");
-		return (-1);
-	}
 	i = 0;
 	while (i < width)
 	{
 		j = 0;
 		while (j < height)
 		{
-			//fdf_put_pixel(minimap->img.img, coord.x * width + i,
-			//coord.y * height + j, coord.color);
-			my_mlx_pixel_put(&minimap->img,  coord.x * width + i,
-					coord.y * height + j, coord.color);
+			my_mlx_pixel_put(&minimap->img,  pixel.x + i, pixel.y + j, pixel.color);
 			j++;
 		}
 		i++;
@@ -40,6 +30,7 @@ int init_minimap_img(t_minimap minimap)
 void draw_minimap(t_minimap *minimap)
 {
 	t_point coord;
+	t_point pixel;
 	int		square_type;
 
 	coord.y = 0;
@@ -50,18 +41,14 @@ void draw_minimap(t_minimap *minimap)
 		{
 			square_type = minimap->map->map[coord.y + minimap->offsets.y][coord.x + minimap->offsets.x] ;
 			if (square_type == 1)
-			{
-				coord.color = fdf_mk_color(125, 0, 255, 0);	
-			}
+				pixel.color = fdf_mk_color(125, 0, 255, 0);	
 			else if (square_type == -1)
-			{
-				coord.color = fdf_mk_color(0, 0, 0, 0);	
-			}
+				pixel.color = fdf_mk_color(0, 0, 0, 0);	
 			else
-			{
-				coord.color = fdf_mk_color(125, 0, 0, 255);	
-			}
-			minimap_draw_square(minimap, coord, minimap->squares_size, minimap->squares_size);
+				pixel.color = fdf_mk_color(125, 0, 0, 255);	
+			pixel.x = coord.x * minimap->squares_size;
+			pixel.y = coord.y * minimap->squares_size;
+			minimap_draw_square(minimap, pixel, minimap->squares_size, minimap->squares_size);
 			coord.x++;
 		}
 		coord.y++;	
